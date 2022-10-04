b4fc15597cf6cb495ac95867a93a145e4b48800e
nickheyer <nick@heyer.app>
2022-10-03 18:08:57 -0700

    Containerized, ready to deploy to DockerHub.

--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
-
 #!/bin/sh
+PORT=6969
 
-IP='ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}''
-PORT=5000
-
-gunicorn --bind $IP:$PORT app:app
+gunicorn --bind 0.0.0.0:$PORT app:app
 echo "Shutting down Gunicorn Server"
